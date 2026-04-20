a059d5de09c5a8ed029e301a70912ac449941bb2
AnaJu-Pires <anaju001@hotmail.com>
2026-04-19 22:26:43 -0300

    ex1-loading finalizado

--- App.tsx
+++ App.tsx
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
+// usei isso pois o SafeAreaView ia ficar deprecated e pesquisei uma alternativa
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
 
 export default function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  // exercício 1: Estado de Loading
+  useEffect(() => {
+    
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" color="#ff0000" />
+        <Text style={styles.loadingText}>Carregando Pokédex...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Pokédex</Text>
+        </View>
+
+        <View style={styles.content}>
+          {}
+          {pokemons.length === 0 ? (
+            <Text style={styles.emptyText}>A lista está vazia.</Text>
+          ) : (
+            <Text>Lista carregada com sucesso!</Text>
+          )}
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    padding: 20,
+    backgroundColor: "#ff0000",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 22,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  loadingText: {
+    marginTop: 10,
+    fontSize: 16,
+  },
+  content: {
+    flex: 1,
+    padding: 20,
+  },
+  emptyText: {
+    textAlign: "center",
+    color: "#666",
   },
 });
